1833aeee8d8c6d70c927151dc9184dd9431f9599
Natally <natallyriqueto@hotmail.com>
2024-03-26 00:23:01 -0400

    add routes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from "./Router.tsx";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
